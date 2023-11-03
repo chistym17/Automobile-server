cde0dddcf3d3d6c8da2d150cf6f1d2546ad88427
chistym17 <mchisty31@gmail.com>
2023-11-03 19:57:55 +0600

    JWT added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express()
+const jwt = require('jsonwebtoken')
 const port=process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
@@ -59,7 +60,13 @@ app.get('/update/:name', async (req, res) => {
 res.send(product)
 })
 
+app.post('/jwt',async(req,res)=>{
+const user=req.body
+const token=jwt.sign(user,process.env.Access_Token,{expiresIn:'1h'})
+console.log(token)
+res.send(token)
 
+})
 
 
 app.post('/addProduct',async(req,res)=>{
